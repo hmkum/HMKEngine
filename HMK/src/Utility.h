42235d0194f60dfa1fee7ad6cdc869befedcb81d
HMK <hakanmertkum@yandex.com.tr>
2015-09-14 10:30:14 +0300

    OpenGL hatalarını görmek için glGetError makrosu eklendi.

--- HMK/src/Utility.h
+++ HMK/src/Utility.h
@@ -1,6 +1,7 @@
 #pragma once
-
+#include <GL/gl3w.h>
 #include <functional>
+#include <string>
 #include "Logger.h"
 #include "Debug.h"
 
@@ -23,3 +24,37 @@ const std::string SKYBOX_TEXTURE_PATH = "data/textures/Cubemaps/";
 const std::string SHADER_PATH = "data/shaders/";
 const std::string MODEL_PATH = "data/models/";
 const std::string TEXTURE_PATH = "data/textures/";
+
+#define CHECK_GL \
+{ \
+	std::string errStr = "OpenGL Error( " + std::string(__FILE__) + ", " + std::to_string(__LINE__) + "): "; \
+	GLenum err = glGetError(); \
+	switch (err) \
+	{ \
+		case GL_INVALID_ENUM: \
+		errStr += "GL_INVALID_ENUM"; \
+		break; \
+		case GL_INVALID_VALUE: \
+		errStr += "GL_INVALID_VALUE"; \
+		break; \
+		case GL_INVALID_OPERATION: \
+		errStr += "GL_INVALID_OPERATION"; \
+		break; \
+		case GL_INVALID_FRAMEBUFFER_OPERATION: \
+		errStr += "GL_INVALID_FRAMEBUFFER_OPERATION"; \
+		break; \
+		case GL_OUT_OF_MEMORY: \
+		errStr += "GL_OUT_OF_MEMORY"; \
+		break; \
+		case GL_STACK_UNDERFLOW: \
+		errStr += "GL_STACK_UNDERFLOW"; \
+		break; \
+		case GL_STACK_OVERFLOW: \
+		errStr += "GL_STACK_OVERFLOW"; \
+		break; \
+		default:\
+		errStr = ""; \
+		break; \
+	} \
+	if (errStr != "") HMK_LOG_ERROR(errStr) HMK_PRINT(errStr) \
+}
